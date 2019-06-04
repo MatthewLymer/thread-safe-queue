af4c694855ba07251a080c73b7dbf2914dea0019
Matthew Lymer <matthew.lymer@tripstack.com>
2019-06-04 11:36:01 -0400

    Add test boilerplate

---
+++ src/ThreadSafeContainers/ThreadSafeQueue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ThreadSafeContainers
+{
+    /// <summary>
+    /// Despite the name of the class, it is currently not thread-safe, and is missing use-cases as defined
+    /// in the test outline.  Some methods may also be missing basic input and output validation that should
+    /// also be taken into consideration
+    /// </summary>
+    public sealed class ThreadSafeQueue<T>
+    {
+        private readonly Queue<T> _queue = new Queue<T>();
+
+        public IEnumerable<T> DequeueAll()
+        {
+            var list = new List<T>();
+
+            while (_queue.Count > 0)
+            {
+                list.Add(_queue.Dequeue());
+            }
+
+            return list;
+        }
+
+        public int Count => _queue.Count;
+
+        public void Enqueue(T value)
+        {
+            _queue.Enqueue(value);
+        }
+
+        public Task<T> DequeueAsync()
+        {
+            var value = _queue.Dequeue();
+            return Task.FromResult(value);
+        }
+    }
+}
---
+++ tests/ThreadSafeContainers.Tests/ThreadSafeQueueTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace ThreadSafeContainers.Tests
+{
+    public sealed class ThreadSafeQueueTests
+    {
+        [Fact]
+        public void ShouldCreateNewInstance()
+        {
+            var queue = new ThreadSafeQueue<string>();
+            Assert.NotNull(queue);
+        }
+
+        [Fact]
+        public void ShouldDefaultToEmpty()
+        {
+            var queue = new ThreadSafeQueue<int>();
+
+            Assert.Equal(0, queue.Count);
+            Assert.Empty(queue.DequeueAll());
+        }
+
+        [Fact]
+        public void ShouldUpdateCountWhenAddingToQueue()
+        {
+            var queue = new ThreadSafeQueue<long>();
+
+            queue.Enqueue(5);
+            queue.Enqueue(42);
+
+            Assert.Equal(2, queue.Count);
+        }
+
+        [Fact]
+        public void ShouldDequeueAllAfterAdded()
+        {
+            var expected = new[]
+            {
+                "hello",
+                "world"
+            };
+
+            var queue = new ThreadSafeQueue<string>();
+
+            queue.Enqueue("hello");
+            queue.Enqueue("world");
+
+            var actual = queue.DequeueAll();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task ShouldDequeueInstantlyIfValueExistsInQueue()
+        {
+            var queue = new ThreadSafeQueue<string>();
+
+            queue.Enqueue("hello");
+            queue.Enqueue("world");
+
+            var actual = await queue.DequeueAsync();
+
+            Assert.Equal("hello", actual);
+        }
+    }
+}
